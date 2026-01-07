5349ca54d2426f00b679a3268cd6ddba14363d33
ErasmoAbreu <erasmoabreu1@gmail.com>
2026-01-07 11:31:33 -0300

    Project created

---
+++ src/test/java/com/webservice/wsp/WspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.wsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
